28220f98e8050a1e385a6b2e8b33b403a351a0a8
Stephan Koenig <stephan.koenig@gmail.com>
2016-01-23 22:25:31 -0800

    feat: Add histogram and add code to Rmd file

---
+++ PA1_template.md
@@ -0,0 +1,33 @@
+# Reproducible Research: Peer Assessment 1
+
+
+
+## Loading and preprocessing the data
+
+File loaded downloaded: Sat Jan 23 22:13:46 2016
+
+## What is mean total number of steps taken per day?
+
+```r
+# Discard row with NAs, group by day, and calculate total steps per day.
+dailyActivity <- na.omit(activity) %>% group_by(date) %>%
+    summarize(totalSteps = sum(steps))
+```
+
+
+![](PA1_template_files/figure-html/Histogram total steps-1.png)\
+
+
+
+The mean is 10766.19 and the median is 10765 for total number of
+steps taken per day.
+
+## What is the average daily activity pattern?
+
+
+
+## Imputing missing values
+
+
+
+## Are there differences in activity patterns between weekdays and weekends?
--- activity_analysis.R
+++ activity_analysis.R
@@ -1,52 +1,80 @@
+# Load packages ================================================================
 # Load all required packages installing any that are missing.
 requiredPackages <- c("dplyr", "ggplot2")
 missingPackages <- !is.element(requiredPackages, installed.packages()[,1])
 missingPackages <- requiredPackages[missingPackages]
-if (length(missingPackages) != 0) { install.packages(missingPackages) }
+if (length(missingPackages) != 0) install.packages(missingPackages)
 lapply(requiredPackages, library, character.only = TRUE)
 rm(requiredPackages, missingPackages)
 
+# Load data ====================================================================
 # Time when copy of activity.zip in this fork was downloaded.
-dateDownloaded <- "Fri Jan 22 16:58:12 2016"
+dateDownloaded <- "Sat Jan 23 22:13:46 2016"
 
 # Download data if not present and update time when the copy was downloaded.
 if (!file.exists("activity.zip") && !file.exists("activity.csv")) {
-        
-        url = paste("https://d396qusza40orc.cloudfront.net/",
-                    "repdata%2Fdata%2Factivity.zip", sep = "")
-        
-        # Determine download.file method based on OS.
-        if (.Platform$OS.type == "unix") {
-                osMethod = "libcurl"
-        } else {
-                osMethod = "wininet"
-        }
-        
-        download.file(url, "activity.zip", method = osMethod)
-        dateDownloaded <- date()
-        rm(url, osMethod)
+    
+    url = paste("https://d396qusza40orc.cloudfront.net/",
+                "repdata%2Fdata%2Factivity.zip", sep = "")
+    
+    # Determine download.file method based on OS.
+    if (.Platform$OS.type == "unix") {
+        osMethod = "libcurl"
+    } else {
+        osMethod = "wininet"
+    }
+    
+    download.file(url, "activity.zip", method = osMethod)
+    dateDownloaded <- date()
+    rm(url, osMethod)
 }
 
 # If .csv data file is not present if not unzip activity.zip
 if (!file.exists("activity.csv")) {
-        unzip("activity.zip")
+    unzip("activity.zip")
 }
 
 # Load .csv file as data frame table
 activity <- tbl_df(read.csv("activity.csv"))
 
 # What is mean total number of steps taken per day?
- 
-# Discard row with NAs, group by day, and calculate total steps per day
-activityTotalPerDay <- na.omit(activity) %>% group_by(date) %>%
-        summarize(totalSteps = sum(steps))
-activityTotalPerDay <- arrange(activityTotalPerDay, totalSteps)
-# Make a histogram of the total number of steps taken each day
 
+# Discard row with NAs, group by day, and calculate total steps per day.
+dailyActivity <- na.omit(activity) %>% group_by(date) %>%
+    summarize(totalSteps = sum(steps))
 
 # Calculate mean and median of the total steps taken per day
-meanSteps <- mean(activityTotalPerDay$totalSteps)
-medianSteps <- median(activityTotalPerDay$totalSteps)
+meanSteps <- mean(dailyActivity$totalSteps)
+medianSteps <- median(dailyActivity$totalSteps)
+
+# Plot data ====================================================================
+# Make a histogram of the total number of steps taken each day.
+# Open graphics device for png.
+png(filename = "./figures/plot3.png",width = 640, height = 480, units = "px",
+    bg = "transparent")
+
+# Determine optimal binwidth for histogram using the Freedman-Diaconis rule.
+binNumber <- nclass.FD(dailyActivity$totalSteps)
+
+# Construct histogram
+histSteps <- ggplot(dailyActivity, aes(totalSteps)) +
+    geom_histogram(aes(y = ..count../sum(..count..)),
+                   origin = min(dailyActivity$totalSteps),
+                   bins = binNumber,
+                   col = "black",
+                   right = TRUE,
+                   fill= "grey",
+                   na.rm = TRUE) +
+    xlim(c(min(dailyActivity$totalSteps),
+           max(dailyActivity$totalSteps))) +
+    ggtitle("Normalized Histogram of Total Steps Per Day") +
+    xlab("total steps per day") +
+    ylab("percent of days")
+print(histSteps)
+
+#close graphics device
+dev.off()
+
 
 # What is the average daily activity pattern?
 # 
